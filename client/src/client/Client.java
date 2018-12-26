87eccf2a8650f6a864567cee55e4e67c18ae0c89
Jatan <rathodjatan@gmail.com>
2018-12-26 19:28:02 +0530

    Removed comments

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package client;
-
-/**
- *
- * @author Jatan
- */
 
 import java.awt.BorderLayout;
 import javax.swing.*;
